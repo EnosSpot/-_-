3e62b682b28fc0a4614af4a64557f76f999d1e95
BuildTools <unconfigured@null.spigotmc.org>
2024-02-20 00:20:04 +0300

    obnova

---
+++ Игра коробка/Map.h
@@ -0,0 +1,15 @@
+#pragma once
+using namespace sf;
+#include <SFML/Graphics.hpp>
+const int HEIGHT_MAP = 8;
+const int WIDTH_MAP = 10;
+std::string TileMap[HEIGHT_MAP] = {
+	"0000000000",
+	"0        0",
+	"0     0  0",
+	"0        0",
+	"0        0",
+	"0        0",
+	"0        0",
+	"0000000000"
+};
